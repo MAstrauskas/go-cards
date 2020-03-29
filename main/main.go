bfcafa823a40d39b5ff389171d1381506f1eee22
Mantas Astrauskas <MAstrauskas4@Gmail.com>
2020-03-29 18:48:42 +0100

    Add saveToFile func

--- main/main.go
+++ main/main.go
@@ -1,20 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
 	// Returns a new slices & does not modify the existing cards slice
 	cards := newDeck()
-
-	// 1st - current hand 0 to 5 (5 not included)
-	// 2nd - handSize to the end (5 to end)
-	currentHand, remainingDeck := deal(cards, 5)
-
-    // cards.print()
-	fmt.Println("------------------------")
-    currentHand.print()
-	fmt.Println("------------------------")
-	remainingDeck.print()
-	fmt.Println("------------------------")
+	_ = cards.saveToFile("my_cards")
 }
 
