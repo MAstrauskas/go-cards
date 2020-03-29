f5e860b2ce20c3675246462b140c9c272aee95cd
Mantas Astrauskas <mastrauskas4@gmail.com>
2020-03-29 11:54:13 +0100

    Refactor newDeck func to deck type

--- main/deck.go
+++ main/deck.go
@@ -6,8 +6,23 @@ import "fmt"
 // of slice of string
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value + " of " + suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
--- main/main.go
+++ main/main.go
@@ -1,15 +1,8 @@
 package main
 
 func main() {
-	cards := deck{newCard(), newCard()}
-
 	// Returns a new slices & does not modify the existing cards slice
-	cards = append(cards, "Ten of Spades")
-
+	cards := newDeck()
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
 
